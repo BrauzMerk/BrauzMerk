12188ca0b1e14fbd5b0856a67c548d9db7b7b7f7
BrauzMerk <hinataiwasaki04@gmail.com>
2026-01-04 15:50:52 +0600

    Update script.js

--- script.js
+++ script.js
@@ -41,9 +41,4 @@ form.addEventListener("submit", async (e) => {
   }
 });
 
-const burger = document.getElementById("burger");
-const sideMenu = document.getElementById("sideMenu");
 
-burger.addEventListener("click", () => {
-  sideMenu.classList.toggle("open");
-});
